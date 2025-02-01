0cefb8316c3662d7409c8ed8f3efb0dceb202716
javadbat <javadbat@gmail.com>
2025-02-01 15:37:13 +0330

    feat: add build config

---
+++ build-config.ts
@@ -0,0 +1,26 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [
+  {
+    name: "jb-input",
+    path: "./lib/index.ts",
+    outputPath: "./jb-input/dist/index.js",
+    umdName: "JBInput",
+    external: ["jb-validation", "jb-form"],
+    globals: {
+      "jb-validation": "JBValidation",
+    },
+  },
+];
+export const reactComponentList: ReactComponentBuildConfig[] = [
+  {
+    name: "jb-input-react",
+    path: "./react/lib/JBInput.tsx",
+    outputPath: "./react/dist/JBInput.js",
+    external: ["jb-input", "prop-types", "react"],
+    globals: {
+      react: "React",
+      "prop-types": "PropTypes",
+    },
+  },
+];
